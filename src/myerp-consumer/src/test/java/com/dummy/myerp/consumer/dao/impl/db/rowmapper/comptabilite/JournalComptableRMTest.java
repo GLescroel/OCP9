020dcfcbf7ab0508fc58a57fe221e7a9b77a63ea
Nicolas <nicolas.lejeune@axa.fr>
2020-05-01 00:11:32 +0200

    fix(): add comma missing

---
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/JournalComptableRMTest.java
@@ -0,0 +1,12 @@
+package com.dummy.myerp.consumer.dao.impl.db.rowmapper.comptabilite;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JournalComptableRMTest {
+
+    @Test
+    public void mapRow() {
+    }
+}
